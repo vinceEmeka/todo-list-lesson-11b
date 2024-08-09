abb8ee81b3b8a7705aeb22afa7cae9c8add30e3e
Vinz <abc@xyz.com>
2024-08-09 22:55:09 +0100

    completed

---
+++ app.js
@@ -0,0 +1,61 @@
+const todoList = [{
+ name: 'make dinner',
+ dueDate: '08-10-2024'
+},
+{
+ name: 'bake bread',
+ dueDate: '08-10-2024'
+}];
+
+renderTodoList();
+
+function renderTodoList() {
+
+
+ let todoListHTML = '';
+
+ for (let i = 0; i < todoList.length; i++) {
+  const todoObject = (todoList[i]);
+  const { name, dueDate } = todoObject;
+  // const dueDate = todoObject.dueDate;
+
+
+  const html = `
+ <div>
+  ${name} </div> 
+  <div>${dueDate} </div>
+  <button onclick="
+  todoList.splice(${i}, 1);
+  renderTodoList()
+  " class = "delete-button">Delete</button>
+  `;
+  todoListHTML += html;
+
+ }
+
+
+ document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+
+}
+
+function addTodo() {
+ const inputElement = document.querySelector('.js-name-input');
+ const name = inputElement.value;
+
+ const dateInputElement = document.querySelector('.js-due-date-input');
+ const dueDate = dateInputElement.value;
+
+ todoList.push({
+  // name: name,
+  // dueDate: dueDate,
+  name,
+  dueDate
+ });
+
+
+ inputElement.value = '';
+
+ renderTodoList();
+
+}
+
